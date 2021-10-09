e69332233bbb0f039833447345ae1695aa456a0d
llamousse <vyue013@gmail.com>
2021-10-09 15:46:24 -0700

    add firebase to app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDrjH10U0tFoknaAjLuHfZLRSc4BvvTs6I",
+  authDomain: "slack-clone-v2-fd17d.firebaseapp.com",
+  projectId: "slack-clone-v2-fd17d",
+  storageBucket: "slack-clone-v2-fd17d.appspot.com",
+  messagingSenderId: "66378386580",
+  appId: "1:66378386580:web:67d578f464e5b64db47881",
+};
